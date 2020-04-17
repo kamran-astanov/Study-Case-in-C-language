507fa5cd955afb5737dd495578fd2baa2a719a31
kastanov-ops435 <59590803+kastanov-ops435@users.noreply.github.com>
2020-04-17 00:13:29 -0400

    Update myfile.h

--- myfile.h
+++ myfile.h
@@ -1,3 +1,30 @@
+/* DIGITAL SIGNATURE(S):
+   ====================
+
+   List the student author(s) of this code below:
+
+   Fullname                    Seneca Email Address
+   --------------------------- ----------------------------
+   1) Kamran Astanov            kastanov@myseneca.ca
+   2) 
+   3)
+
+   +--------------------------------------------------------+
+   |                FILE: file_helper.c                     |
+   +--------------------------------------------------------+
+   |           2  0  2  0  ~  W  I  N  T  E  R              |
+   |                 I  P  C  :  B  T  P                    |
+   |                 1  4  4  :  1  0  0                    |
+   |              FINAL ASSESSMENT PART - 2                 |
+   |                                                        |
+   |     S  E  N  E  C  A            C  O  L  L  E  G  E    |
+   |                                                        |
+   |  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  |
+   |  !!!      DO NOT ALTER THE CONTENTS BELOW         !!!  |
+   |  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  |
+   +--------------------------------------------------------+ */
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
